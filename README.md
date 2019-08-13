5ed796aa56591da42fecd46763591b697c1bd33a
Rokadeshubham <shubhamrokade211@gmail.com>
2019-08-13 18:52:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3jp1a2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3jp1a2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  constructor(private fb:FormBuilder){
+
+  }
+  employeeForm:FormGroup;
+  ngOnInit(){
+    this.employeeForm=this.fb.group({
+      fullName:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
+      email:['',[Validators.required,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')]],
+        skillName:['',[Validators.required]],
+        experienceInYears:['',[Validators.required]]
+     
+     
+    })
+  }
+}
